96e94772e9d7090ff05cdb666b174a6fb2da3639
Ryan Leung <rwcleung@gmail.com>
2014-10-21 11:39:04 +0800

    Add `goLoginCaptcha` method to `MainActivity`.

--- src/com/thirtysparks/apple/bot/MainActivity.java
+++ src/com/thirtysparks/apple/bot/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.bumptech.glide.Glide;
@@ -12,6 +14,9 @@ import java.util.GregorianCalendar;
 
 public class MainActivity extends Activity {
     private static final String TAG = "MainActivity";
+    private static final String APPLE_ID = "ENTER_APPLIE_ID";
+    private static final String PASSWORD = "ENTER_PASSWORD";
+
     ReserveWorker reserveWorker;
 
     TextView tvMsg;
@@ -24,6 +29,14 @@ public class MainActivity extends Activity {
         tvMsg = (TextView)findViewById(R.id.tv_msg);
 
         reserveWorker = new ReserveWorker();
+
+        findViewById(R.id.btn_captcha).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goLoginCaptcha();
+            }
+        });
+
         goFrontPage();
     }
 
@@ -56,17 +69,48 @@ public class MainActivity extends Activity {
                     tvMsg.setText("Failed");
                 }
 
-                getCaptcha();
+                goGetCaptcha();
 
             }
         }.execute();
     }
 
 
-    private void getCaptcha() {
+    private void goGetCaptcha() {
         tvMsg.setText("Getting captcha");
         ImageView captchaImageView = (ImageView)findViewById(R.id.iv_captcha);
         String imageUrl = "https://signin.apple.com/IDMSWebAuth/imageCaptcha/942#" + GregorianCalendar.getInstance().getTime().getTime();
         Glide.with(MainActivity.this).load(imageUrl).into(captchaImageView);
+    }
+
+
+
+    private void goLoginCaptcha() {
+        tvMsg.setText("Submitting captcha");
+
+        final String captchaInput = ((EditText) findViewById(R.id.et_captcha)).getText().toString();
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... params)  {
+                String string = null;
+                try {
+                    string = reserveWorker.loginWithCaptcha(captchaInput, APPLE_ID, PASSWORD);
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+
+                return string;
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                if ("https://reserve-hk.apple.com/HK/en_HK/reserve/iPhone?execution=e1s2".equals(s)) {
+                    tvMsg.setText("Apple ID Login successfully");
+                } else {
+                    tvMsg.setText("Error: Apple ID Login failed");
+                }
+            }
+        }.execute();
     }
 }
